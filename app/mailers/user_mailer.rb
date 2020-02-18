89ed38109fd2e960ff6a06cebcf266ccf48d158f
BDiumula <diumula.benedicte@hotmail.fr>
2020-02-18 13:00:53 +0100

    with mailer attendance

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-default from: 'no-reply@monsite.fr'
+default from: 'no-reply@benestore.com'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
@@ -10,5 +10,16 @@ default from: 'no-reply@monsite.fr'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+  end
+
+  def attendance_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Un nouvel évènement a été créé !') 
   end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ApplicationRecord
 	belongs_to :user
-	belongs_to :attendance
+	belongs_to :event
+	after_create :attendance_email_send
+
+  def attendance_email_send
+    UserMailer.attendance_email(self).deliver_now
+  end
 end
